039dfeca7d868a1b4db3354342b999e416d48262
Alkibijad <aleksandar04081@gmail.com>
2022-01-13 15:15:18 -0800

    added bank app

---
+++ BankApp2.1/landingPage/bankAppResetForm.js
@@ -0,0 +1,6 @@
+function resetForm() {
+  let signUpInputs = document.querySelectorAll("input");
+  for (let i = 0; i < signUpInputs.length; i++) {
+    signUpInputs[i].value = "";
+  }
+}
---
+++ BankApp2.1/landingPage/bankAppSignUp.js
@@ -0,0 +1,8 @@
+function signUp() {
+  viewEnterPage.style.display = "none";
+  viewSignUp.style.display = "block";
+  viewLogIn.style.display = "none";
+
+  let btnSubmit = document.querySelector(".btnSubmit");
+  btnSubmit.addEventListener("click", formVerification);
+}
